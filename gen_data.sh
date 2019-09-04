3d4f86cd5639cba326393bf72aabb08eb8da92cd
Brian Vanderwende <vanderwb@ucar.edu>
2019-09-04 15:04:07 -0600

    Reverted default format to match qstat

--- gen_data.sh
+++ gen_data.sh
@@ -3,31 +3,34 @@
 TMPPATH=/dev/shm
 ENDPATH=/glade/work/csgteam/qs_cache
 
+mkdir -p $ENDPATH
 cd $TMPPATH
 
 # Get data from PBS (must run as csgteam)
-qstat -a -1 -n -s -w -x | sed '1,5d' > newlist-wide.qsdat &
-qstat -1 -n -s -x | sed '1,5d' > newlist-default.qsdat &
+sudo -u csgteam /opt/pbs/bin/qstat -a -1 -n -s -w -x | sed '1,5d' > newlist-wide.dat &
+sudo -u csgteam /opt/pbs/bin/qstat -1 -n -s -x | sed '1,5d' > newlist-info.dat &
+sudo -u csgteam /opt/pbs/bin/qstat -x | sed '1,5d' > newlist-default.dat &
 
 time wait
 
 # Poor-man's sync
-mv newlist-wide.qsdat commlist-wide-nodes.qsdat
-mv newlist-default.qsdat commlist-default-nodes.qsdat
+mv newlist-wide.dat commlist-wide-nodes.dat
+mv newlist-info.dat commlist-info-nodes.dat
+mv newlist-default.dat joblist-default.dat
 
 # Get versions without admin comment
-grep -v '^ ' commlist-wide-nodes.qsdat > joblist-wide-nodes.qsdat &
-grep -v '^ ' commlist-default-nodes.qsdat > joblist-default-nodes.qsdat &
+grep -v '^ ' commlist-wide-nodes.dat > joblist-wide-nodes.dat &
+grep -v '^ ' commlist-info-nodes.dat > joblist-info-nodes.dat &
 
 time wait
 
 # Get versions without nodelist
-sed -r 's/([0-9].*) [-,r].*/\1/' commlist-wide-nodes.qsdat > commlist-wide.qsdat &
-sed -r 's/([0-9].*) [-,r].*/\1/' commlist-default-nodes.qsdat > commlist-default.qsdat &
-sed -r 's/([0-9].*) [-,r].*/\1/' joblist-wide-nodes.qsdat > joblist-wide.qsdat &
-sed -r 's/([0-9].*) [-,r].*/\1/' joblist-default-nodes.qsdat > joblist-default.qsdat &
+sed -r 's/([0-9].*) [-,r].*/\1/' commlist-wide-nodes.dat > commlist-wide.dat &
+sed -r 's/([0-9].*) [-,r].*/\1/' commlist-info-nodes.dat > commlist-info.dat &
+sed -r 's/([0-9].*) [-,r].*/\1/' joblist-wide-nodes.dat > joblist-wide.dat &
+sed -r 's/([0-9].*) [-,r].*/\1/' joblist-info-nodes.dat > joblist-info.dat &
 
 time wait
 
 # Move files to final storage
-mv *.qsdat $ENDPATH
+mv *.dat $ENDPATH
